da9115fe6a6d9b0fff9e7aa3f4f5a080a70e5d9b
raaaaaaaaaf <rridad@ssct.edu.ph>
2025-03-24 19:32:42 +0800

    change apiUrl

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
-const apiUrl = "https://django-react-backend-nzrz.onrender.com"
+const apiUrl = "https://django-react-backend-nzrz.onrender.com/api";
+
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
